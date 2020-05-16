6856a44fe02248351b20a9fdfecc8da7abf51278
constuction-dev <minesh_t@hotmail.com>
2020-05-16 18:10:12 +0100

    Updates

---
+++ precache-manifest.d3187ae372057f8c434cd9ed1b11b572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e08e9ba613830e68ca05c6e5e12cc4",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b1bf10cff7cf9cf6950c",
+    "url": "/bmi-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ae4458cb504c702364f",
+    "url": "/bmi-calculator/static/css/main.c55bc020.chunk.css"
+  },
+  {
+    "revision": "b1bf10cff7cf9cf6950c",
+    "url": "/bmi-calculator/static/js/2.9ab53e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bmi-calculator/static/js/2.9ab53e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae4458cb504c702364f",
+    "url": "/bmi-calculator/static/js/main.6a61d712.chunk.js"
+  },
+  {
+    "revision": "b0d9f7a354bbb539ef1e",
+    "url": "/bmi-calculator/static/js/runtime-main.7cfc36d9.js"
+  }
+]);
